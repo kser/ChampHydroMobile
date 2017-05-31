a15c8cf8801777b2c3a7bf05c2998584d00e5be9
Kyle Serres <kyle.serres@hpe.com>
2017-05-31 00:27:28 -0500

    add josh and jerry accounts / images / data

--- src/mocks/mock-districts.ts
+++ src/mocks/mock-districts.ts
@@ -1,31 +1,228 @@
 import { District } from '../models/district';
 import { Project } from '../models/project';
 
-function getBase64Image(img) {
-  var canvas = document.createElement("canvas");
-  canvas.width = img.width;
-  canvas.height = img.height;
-  var ctx = canvas.getContext("2d");
-  ctx.drawImage(img, 0, 0);
-  var dataURL = canvas.toDataURL("image/jpg");
-  return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
 
 export const DISTRICTS:District[] = [
+  //JERRY Districts
   new District(
-    "HC MUD NO. 468",
-    getBase64Image("assets/img/DistrictMaps/mud-468-map.jpg"),
+    "Cimarron",
+    "assets/img/DistrictMaps/cimarron.jpg",
     [
-    new Project("Vintage Southeast Detention Pond")
+      new Project("Perch Rd Pond", "assets/img/DistrictMaps/Perch Rd Pond.jpg")
     ]
     ),
-  new District(
-    "HC Base 64",
-    // "assets/img/DistrictMaps/hc-mud-468.bin",
-    "assets/img/DistrictMaps/mud-468-map.jpg",
+    new District(
+    "CINCO SW 1",
+    "assets/img/DistrictMaps/CINCO SW 1.jpg",
     [
-    new Project("Vintage Southeast Detention Pond")
+      new Project("Channel DE", "assets/img/DistrictMaps/Channel DE.jpg"),
+      new Project("Channel DSE", "assets/img/DistrictMaps/Channel DSE.jpg"),
+      new Project("Channel DS", "assets/img/DistrictMaps/Channel DS.jpg"),
+      new Project("WQP", "assets/img/DistrictMaps/WQP.jpg"),
+      new Project("Channel DN", "assets/img/DistrictMaps/Channel DN.jpg"),
+      new Project("Channel DW", "assets/img/DistrictMaps/Channel DW.jpg"),
+      new Project("1463 Ditch", "assets/img/DistrictMaps/1463 Ditch.jpg"),
+      new Project("John Sauder Memorial Park")//, "assets/img/DistrictMaps/John Sauder Memorial Park.jpg"),
     ]
-    )
+    ),
+    new District(
+    "FBC MUD 118",
+    "assets/img/DistrictMaps/FBC MUD 118.jpg",
+    [
+      new Project("Waterside Estates", "assets/img/DistrictMaps/Waterside Estates.jpg")
+    ]
+    ),
+    new District(
+    "FBC MUD 2",
+    "assets/img/DistrictMaps/FBC MUD 2.jpg",
+    [
+      new Project("Florence Rd Detention", "assets/img/DistrictMaps/Florence Rd Detention.jpg"),
+      new Project("Belknap Rd Detention", "assets/img/DistrictMaps/Belknap Rd Detention.jpg"),
+      new Project("Eldridge Park Village", "assets/img/DistrictMaps/Eldridge Park Village.jpg"),
+      new Project("Water Plant 1", "assets/img/DistrictMaps/Water Plant 1.jpg"),
+      new Project("Water Plant 2", "assets/img/DistrictMaps/Water Plant 2.jpg"),
+      new Project("Towne West Drainage", "assets/img/DistrictMaps/Towne West Drainage.jpg"),
+      new Project("Landscape Areas 1-5"),// "assets/img/DistrictMaps/Landscape Areas 1-5.jpg"),
+      new Project("Burney Esplanades"),// "assets/img/DistrictMaps/Burney Esplanades.jpg"),
+      new Project("West Bellfort Esplanades"),// "assets/img/DistrictMaps/West Bellfort Esplanades.jpg"),
+    ]
+    ),
+    new District(
+    "FBC MUD 34/35",
+    "assets/img/DistrictMaps/FBC MUD 34-35.jpg",
+    [
+      new Project("Little Prong Creek South", "assets/img/DistrictMaps/Little Prong Creek South.jpg")
+    ]
+    ),
+    new District(
+    "HFB MUD 5",
+    "assets/img/DistrictMaps/HFB MUD 5.jpg",
+    [
+      new Project("Falcon Point", "assets/img/DistrictMaps/Falcon Point.jpg"),
+      new Project("Katy Creek Ranch", "assets/img/DistrictMaps/Katy Creek Ranch.jpg"),
+      new Project("Katy Creek Ranch Swale", "assets/img/DistrictMaps/Katy Creek Ranch Swale.jpg"),
+    ]
+    ),
+    new District(
+    "HC MUD 287",
+    "assets/img/DistrictMaps/HC MUD 287.jpg",
+    [
+      new Project("Waterstone Ph I", "assets/img/DistrictMaps/Waterstone Ph I.jpg"),
+      new Project("Waterstone Ph II", "assets/img/DistrictMaps/Waterstone Ph II.jpg"),
+      new Project("Waterstone Ph III", "assets/img/DistrictMaps/Waterstone Ph III.jpg"),
+      new Project("Waterstone Ph 4/5", "assets/img/DistrictMaps/Waterstone Ph 4-5.jpg"),
+      new Project("Waterstone Sec 6", "assets/img/DistrictMaps/Waterstone Sec 6.jpg"),
+      new Project("WWTP", "assets/img/DistrictMaps/287-WWTP.jpg"),
+      new Project("Morton Ranch Sec 5 & 7", "assets/img/DistrictMaps/Morton Ranch Sec 5 7.jpg"),
+      new Project("Morton Ranch Sec 6 & 8", "assets/img/DistrictMaps/Morton Ranch Sec 6 8.jpg"),
+    ]
+    ),
+    new District(
+    "HC MUD 355",
+    "assets/img/DistrictMaps/HC MUD 355.jpg",
+    [
+      new Project("Lakes of Parkway")//, "assets/img/DistrictMaps/Lakes of Parkway.jpg")
+    ]
+    ),
+    new District(
+    "HC MUD 529",
+    "assets/img/DistrictMaps/HC MUD 529.jpg",
+    [
+      new Project("Parkway Terrace 1 & 2", "assets/img/DistrictMaps/Parkway Terrace 1 2.jpg"),
+      new Project("The Parkway at Eldridge 3", "assets/img/DistrictMaps/The Parkway at Eldridge 3.jpg"),
+      new Project("Parkway at Eldridge")//, "assets/img/DistrictMaps/Parkway at Eldridge.jpg"),
+    ]
+    ),
+    new District(
+    "RIDGEWOOD LANDSCAPING",
+    "assets/img/DistrictMaps/RIDGEWOOD LANDSCAPING.jpg",
+    [
+      new Project("Cinco NW North/South Slopes")//, "assets/img/DistrictMaps/Cinco NW North South Slopes.jpg")
+    ]
+    ),
+    new District(
+    "WESTON MUD",
+    "assets/img/DistrictMaps/WESTON MUD.jpg",
+    [
+      new Project("Mason Creek Detention", "assets/img/DistrictMaps/Mason Creek Detention.jpg"),
+      new Project("WWTP"),//, "assets/img/DistrictMaps/Weston-WWTP.jpg"),
+      new Project("Westgreen Rd Detention", "assets/img/DistrictMaps/Westgreen Rd Detention.jpg"),
+      new Project("0 Fort Station Pond", "assets/img/DistrictMaps/0 Fort Station Pond.jpg"),
+      new Project("East Site Detention", "assets/img/DistrictMaps/East Site Detention.jpg"),
+    ]
+    ),
+    new District(
+    "WFDD",
+    "assets/img/DistrictMaps/WFDD.jpg",
+    [
+      new Project("Willow Fork Drainage"),//, "assets/img/DistrictMaps/Willow Fork Drainage.jpg"),
+      new Project("Central Green", "assets/img/DistrictMaps/Central Green.jpg"),
+      new Project("Exploration Park", "assets/img/DistrictMaps/Exploration Park.jpg"),
+      new Project("Falcon Landing Trail", "assets/img/DistrictMaps/Falcon Landing Trail.jpg"),
+      new Project("Willow Fork Park")//, "assets/img/DistrictMaps/Willow Fork Park.jpg"),
+    ]
+    ),
+
+//JOSH
+    new District(
+    "FBC MUD 158",
+    "assets/img/DistrictMaps/FBC MUD 158.jpg",
+    [
+      new Project("River Run Drainage Channel", "assets/img/DistrictMaps/River Run Drainage Channel.jpg"),
+      new Project("River Run Lift Station", "assets/img/DistrictMaps/River Run Lift Station.jpg"),
+      new Project("River's Mist", "assets/img/DistrictMaps/Rivers Mist.jpg"),
+    ]
+    ),
+    new District(
+    "FBC MUD 159",
+    "assets/img/DistrictMaps/FBC MUD 159.jpg",
+    [
+      new Project("Oaks of Rosenberg", "assets/img/DistrictMaps/Oaks of Rosenberg.jpg"),
+    ]
+    ),
+    new District(
+    "FBC MUD 167",
+    "assets/img/DistrictMaps/FBC MUD 167.jpg",
+    [
+      new Project("Detention Pond 1", "assets/img/DistrictMaps/Detention Pond 1.jpg"),
+      new Project("Detention Pond 3", "assets/img/DistrictMaps/Detention Pond 3.jpg"),
+    ]
+    ),
+    new District(
+    "HC MUD 106",
+    "assets/img/DistrictMaps/HC MUD 106.jpg",
+    [
+      new Project("Commercial Mix Pond"),//, "assets/img/DistrictMaps/Commercial Mix Pond.jpg"),
+      new Project("Eagle Springs North Channel")//, "assets/img/DistrictMaps/Eagle Springs North Channel.jpg"),
+    ]
+    ),
+    new District(
+    "HC MUD 157",
+    "assets/img/DistrictMaps/HC MUD 157.jpg",
+    [
+      new Project("FM 529 Retail Center Detention Pond")//, "assets/img/DistrictMaps/FM 529 Retail Center Detention Pond.jpg"),
+    ]
+    ),
+    new District(
+    "HC MUD 238",
+    "assets/img/DistrictMaps/HC MUD 238.jpg",
+    [
+      new Project("Oakridge Detention Pond"),//, "assets/img/DistrictMaps/Detention Pond 1.jpg"),
+      new Project("Barker's Crossing Pond", "assets/img/DistrictMaps/Barkers Crossing Pond.jpg"),
+      new Project("Lake Ridge Pond", "assets/img/DistrictMaps/Lake Ridge Pond.jpg"),
+      new Project("Facilities"),// "assets/img/DistrictMaps/Facilities.jpg"),
+      new Project("Harris County Channels")//, "assets/img/DistrictMaps/Harris County Channels.jpg"),
+    ]
+    ),
+    new District(
+    "HC MUD 285",
+    "assets/img/DistrictMaps/HC MUD 285.jpg",
+    [
+      new Project("New Forest Channel", "assets/img/DistrictMaps/New Forest Channel.jpg"),
+      new Project("Carpenter's Landing", "assets/img/DistrictMaps/Carpenters Landing.jpg"),
+      new Project("Liberty Lakes", "assets/img/DistrictMaps/Liberty Lakes.jpg"),
+      new Project("New Forest Pond", "assets/img/DistrictMaps/New Forest Pond.jpg"),
+    ]
+    ),
+    new District(
+    "HC MUD 290",
+    "assets/img/DistrictMaps/HC MUD 290.jpg",
+    [
+      new Project("Eagle Springs", "assets/img/DistrictMaps/Eagle Springs.jpg"),
+      new Project("Eagle Springs Detention Pond"),// "assets/img/DistrictMaps/Barkers Crossing Pond.jpg"),
+      new Project("Williams Gully")//, "assets/img/DistrictMaps/Lake Ridge Pond.jpg"),
+    ]
+    ),
+    new District(
+    "HC MUD 374",
+    "assets/img/DistrictMaps/HC MUD 374.jpg",
+    [
+      new Project("Channel A", "assets/img/DistrictMaps/Channel A.jpg"),
+      new Project("Drop Pool 1"),// "assets/img/DistrictMaps/Drop Pool 1.jpg"),
+      new Project("Drop Pool 2"),// "assets/img/DistrictMaps/Drop Pool 2.jpg"),
+      new Project("Channel B", "assets/img/DistrictMaps/Channel B.jpg"),
+      new Project("Mound Road Pond", "assets/img/DistrictMaps/Mound Road Pond.jpg"),
+      new Project("Channel C", "assets/img/DistrictMaps/Channel C.jpg"),
+    ]
+    ),
+    new District(
+    "HC MUD 433",
+    "assets/img/DistrictMaps/HC MUD 433.jpg",
+    [
+      new Project("IDC 1", "assets/img/DistrictMaps/IDC 1.jpg"),
+      new Project("IDC 2", "assets/img/DistrictMaps/IDC 2.jpg"),
+      new Project("IDC 3", "assets/img/DistrictMaps/IDC 3.jpg"),
+      new Project("IDC 4", "assets/img/DistrictMaps/IDC 4.jpg"),
+      new Project("IDC 5", "assets/img/DistrictMaps/IDC 5.jpg"),
+      new Project("Drainage Channel Phase II", "assets/img/DistrictMaps/Drainage Channel Phase II.jpg"),
+    ]
+    ),
+    new District(
+    "HC MUD 468",
+    "assets/img/DistrictMaps/HC MUD 468.jpg",
+    [
+      new Project("Vintage Southeast Detention Pond")//, "assets/img/DistrictMaps/Vintage Southeast Detention Pond.jpg"),
+    ]
+    ),
   ];
 
