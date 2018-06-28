a5b06d95af52c55ca47d6c49ff437d65dc2a94e9
Kyle Serres <kser@users.noreply.github.com>
2018-06-28 08:37:32 -0500

    v8 nsphotolibraryaddusagedescr added

--- src/pages/project-detail/project-detail.ts
+++ src/pages/project-detail/project-detail.ts
@@ -119,24 +119,6 @@ export class ProjectDetailPage {
     actionSheet.present();
   }
 
-  testCamera(){
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE,
-      saveToPhotoAlbum: true,     
-    }
-    
-    this.camera.getPicture(options).then((imageData) => {
-     // imageData is either a base64 encoded string or a file URI
-     // If it's base64 (DATA_URL):
-     let base64Image = 'data:image/jpeg;base64,' + imageData;
-     console.log(base64Image);
-    }, (err) => {
-     // Handle error
-    });
-  }
 
   takePicture(sourceType, saveToAlbum, pageNum, photoNum) {
 
